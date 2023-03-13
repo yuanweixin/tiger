1184d713586d81c49f523821fb872f3c7ff18137
Wei Xin Yuan <weixinyuan6@gmail.com>
2023-03-13 00:46:58 +0000

    Add a integ test to cover the parsing good and bad input files

---
+++ tests/parsing.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use lrlex::{lrlex_mod, DefaultLexeme, LRLexError};
+use lrpar::{lrpar_mod, NonStreamingLexer, Span};
+
+lrlex_mod!("tiger.l");
+lrpar_mod!("tiger.y");
+
+#[test]
+fn test_good() {
+    let paths = fs::read_dir("tests/tiger_programs/parsing/good").unwrap();
+
+    for path in paths {
+        let p = path.unwrap().path();
+        println!("path is {}", p.display());
+        let input = fs::read_to_string(p).unwrap();
+        let lexerdef = tiger_l::lexerdef();
+        let lexer = lexerdef.lexer(&input);
+        let (_res, errs) = tiger_y::parse(&lexer);
+        assert_eq!(errs.len(), 0);
+    }
+}
+
+#[test]
+fn test_bad() {
+    let paths = fs::read_dir("tests/tiger_programs/parsing/bad").unwrap();
+
+    for path in paths {
+        let p = path.unwrap().path();
+        println!("path is {}", p.display());
+        let input = fs::read_to_string(p).unwrap();
+        let lexerdef = tiger_l::lexerdef();
+        let lexer = lexerdef.lexer(&input);
+        let (_res, errs) = tiger_y::parse(&lexer);
+        assert!(errs.len() > 0);
+    }
+}
