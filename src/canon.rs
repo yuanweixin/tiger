2de6eba8404ac1e03165c4d3cae650fefaab0c31
Wei <weixinyuan6@gmail.com>
2023-07-07 10:24:55 -0400

    add the basic block stuff

--- src/canon.rs
+++ src/canon.rs
@@ -1,12 +1,6 @@
-use crate::{
-    ir::IrExp,
-    ir::IrExp::*,
-    ir::IrStm,
-    ir::IrStm::*,
-    temp::{GenTemporary, Label},
-};
+use crate::{temp, ir::IrExp, ir::IrExp::*, ir::IrStm, ir::IrStm::*, temp::GenTemporary};
 
-use std::collections::VecDeque;
+use std::{collections::VecDeque, iter::Peekable};
 
 #[inline]
 fn nop() -> IrStm {
@@ -137,7 +131,11 @@ fn lift_stm(s: IrStm, gen: &mut GenTemporary) -> IrStm {
                     gen,
                 ),
                 (Eseq(s, e), src) => lift_stm(Seq(s, Box::new(Move(e, Box::new(src)))), gen),
-                (a, b) => reorder_stm(VecDeque::with_capacity(0), |_| Move(Box::new(a), Box::new(b)), gen),
+                (a, b) => reorder_stm(
+                    VecDeque::with_capacity(0),
+                    |_| Move(Box::new(a), Box::new(b)),
+                    gen,
+                ),
             }
         }
         Exp(ebox) => match *ebox {
@@ -202,6 +200,10 @@ fn lift_exp(e: IrExp, gen: &mut GenTemporary) -> (IrStm, IrExp) {
 }
 
 pub fn linearize(i: IrStm, gen: &mut GenTemporary) -> Vec<IrStm> {
+    // From an arbitrary Tree statement, produce a list of cleaned trees
+    //    satisfying the following properties:
+    //       1.  No SEQ's or ESEQ's
+    //       2.  The parent of every CALL is an EXP(..) or a MOVE(TEMP t,..)
     fn helper(i: IrStm, mut rest: Vec<IrStm>) -> Vec<IrStm> {
         match i {
             Seq(a, b) => helper(*a, helper(*b, rest)),
@@ -214,10 +216,77 @@ pub fn linearize(i: IrStm, gen: &mut GenTemporary) -> Vec<IrStm> {
     helper(lift_stm(i, gen), Vec::new())
 }
 
-pub fn basic_blocks(_: Vec<IrStm>) -> (Vec<Vec<IrStm>>, Label) {
-    todo!()
+type Block = Vec<IrStm>;
+pub fn basic_blocks(stmts: Vec<IrStm>, gen: &mut GenTemporary) -> Vec<Block> {
+    // From a list of cleaned trees, produce a list of
+    //  basic blocks satisfying the following properties:
+    //       1. and 2. as above;
+    //       3.  Every block begins with a LABEL;
+    //           4.  A LABEL appears only at the beginning of a block;
+    //           5.  Any JUMP or CJUMP is the last stm in a block;
+    //           6.  Every block ends with a JUMP or CJUMP;
+    //        Also produce the "label" to which control will be passed
+    //        upon exit.
+    let mut blist = Vec::new();
+    let mut this_block = Vec::new();
+
+    let mut iter =  stmts.into_iter().peekable() ;
+    while iter.len() > 0 {
+        // add the start label.
+        match iter.peek() {
+            None => {},
+            Some(Label(..)) => {
+                this_block.push(iter.next().unwrap());
+            },
+            Some(_) => {
+                let l = gen.new_label();
+                this_block.push(Label(l));
+            }
+        }
+        // add the rest in the current block until we hit a Jump or Cjump, or a Label, or run out.
+        while let Some(stmt) = iter.peek() {
+            match stmt {
+                Jump(..) | Cjump(..) => {
+                    // clean end of block
+                    this_block.push(iter.next().unwrap());
+                    blist.push(this_block);
+                    this_block = Vec::new();
+                    break;
+                }
+                Label(l) => {
+                    // need to make up a jump to the label of the next block
+                    this_block.push(Jump(Box::new(Name(*l)), vec![*l]));
+                    blist.push(this_block);
+                    this_block = Vec::new();
+                    break;
+                }
+                _ => {
+                    // normal stmt, add it.
+                    this_block.push(iter.next().unwrap());
+                }
+            }
+        }
+        // did we run out of stmts before encountering Jump or Cjump? If so add a end label.
+        if this_block.len() > 0 {
+            let end_label = gen.new_label();
+            this_block.push(Jump(Box::new(Name(end_label)), vec![end_label]));
+            blist.push(this_block);
+            this_block = Vec::with_capacity(0); // need this to satisfy borrow checker.
+
+            assert!(iter.len() == 0); // sanity check
+        }
+    }
+    blist
 }
 
-pub fn trace_schedule(_: Vec<Vec<IrStm>>, _: Label) -> Vec<IrStm> {
+pub fn trace_schedule(_: Vec<Vec<IrStm>>, _: temp::Label) -> Vec<IrStm> {
+    // From a list of basic blocks satisfying properties 1-6,
+    //         along with an "exit" label,
+    //     produce a list of stms such that:
+    //       1. and 2. as above;
+    //           7. Every CJUMP(_,t,f) is immediately followed by LABEL f.
+    //         The blocks are reordered to satisfy property 7; also
+    //     in this reordering as many JUMP(T.NAME(lab)) statements
+    //         as possible are eliminated by falling through into T.LABEL(lab).
     todo!()
 }
