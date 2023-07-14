9a32a6fb4c0d347efe2e9f1400721c7b1ca99215
Wei <weixinyuan6@gmail.com>
2023-07-13 20:50:06 -0400

    add cmdline options

--- src/tc.rs
+++ src/tc.rs
@@ -1,23 +1,23 @@
 use std::env;
 
+use clap::{arg, command, Arg, ArgAction};
 use lrlex::lrlex_mod;
 use lrpar::lrpar_mod;
-use clap::{command, Arg};
 use std::fs;
 
-mod int_types;
-mod util;
 mod absyn;
-mod symtab;
-mod symbol;
-mod temp;
-mod ir;
-mod translate;
-mod frame;
-mod semant;
-mod escape;
-mod canon;
 mod assem;
+mod canon;
+mod escape;
+mod frame;
+mod int_types;
+mod ir;
+mod semant;
+mod symbol;
+mod symtab;
+mod temp;
+mod translate;
+mod util;
 
 use crate::frame::x86_64::x86_64_Frame;
 
@@ -25,20 +25,24 @@ lrlex_mod!("tiger.l");
 lrpar_mod!("tiger.y");
 
 fn main() {
-
     let matches = command!()
         .arg(Arg::new("file"))
+        .arg(arg!(--irgen "dump the ir, after constant folding (if enabled), to file named <basename>.ir").required(false))
+        .arg(arg!(--irrun "run the ir through interpreter"))
+        .arg(arg!(--"report-opts" "output the list of optimizations supported by compiler"))
+        .arg(arg!(--optir <phase> "output the ir after <phase>, where <phase> can be initial|final"))
+        .arg(arg!(-O<opt> "enable optimization <opt>, can be specified multiple times. other optimizations are disabled unless enabled. <opt> is the list of optimizations output from --report-opts. -O0 means disable all optimizations.").action(ArgAction::Append))
         .get_matches();
 
     let file = matches.get_one::<String>("file");
     if let None = file {
-            eprintln!("Missing required argument <file>");
-            util::exit(util::ReturnCode::ExUsage);
+        eprintln!("Missing required argument <file>");
+        util::exit(util::ReturnCode::ExUsage);
     }
 
     let input = fs::read_to_string(file.unwrap());
 
-    if let Err( ref err) = input {
+    if let Err(ref err) = input {
         eprintln!("Unable to read file {}, got error {}", file.unwrap(), err);
         util::exit(util::ReturnCode::OtherErrors);
     }
@@ -56,7 +60,8 @@ fn main() {
         util::exit(util::ReturnCode::SyntaxError);
     }
 
-    let ir = semant::translate::<x86_64_Frame>(input.as_ref().unwrap(), &mut ast_opt.unwrap().unwrap());
+    let ir =
+        semant::translate::<x86_64_Frame>(input.as_ref().unwrap(), &mut ast_opt.unwrap().unwrap());
 
     if ir.is_err() {
         println!("type checking failed");
@@ -65,8 +70,3 @@ fn main() {
 
     println!("{:#?}", ir.unwrap());
 }
-
-
-
-
-
