e6c39602b0dec9d4048c9f5fc50436c746afecb6
Wei <weixinyuan6@gmail.com>
2023-07-26 16:25:04 -0400

    make run e2e test script print out the error code first

--- tc/tests/run_e2e_test.sh
+++ tc/tests/run_e2e_test.sh
@@ -28,9 +28,16 @@ do
     echo "Linking with runtime: "$asm_name
     gcc -fPIE $RUNTIME $asm_name
 
-    echo "Invoking the final program with timeout..."
-    timeout 5s ./a.out
+    echo "Invoking the compiled tiger program with timeout..."
+    result=0
+    # || trick from https://stackoverflow.com/questions/26675681/how-to-check-the-exit-status-using-an-if-statement
+    sh -c "timeout 5s ./a.out" || result=$?
 
+    if [ $result -ne 0 ]
+    then
+        echo "tiger program failed, got status "$result
+        exit $result
+    fi
     echo "Succeeded for :"$tig
 
     i=$((i+1))
